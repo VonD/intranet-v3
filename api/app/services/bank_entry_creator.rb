33118a2b902344c318cacf6d8a638392b6b726d0
Paul Vonderscher <paul.vonderscher@gmail.com>
2014-08-31 17:52:46 +0200

    Extract base functionalities into a BankEntryService

--- api/app/services/bank_entry_creator.rb
+++ api/app/services/bank_entry_creator.rb
@@ -1,17 +1,9 @@
-class BankEntryCreator
-
-	attr_accessor :bank_entry, :accounting_entries
+class BankEntryCreator < BankEntryService
 
 	def initialize params={}
-		@params = params
 		@bank_entry = BankEntry.new
 		@accounting_entries = []
-		transfer_bank_entry_params
-		transfer_accounting_entries_params
-	end
-
-	def save
-		validate && persist
+		super
 	end
 
 private
@@ -45,46 +37,6 @@ private
 			amounts_match?,
 			respects_date_sequence?
 		].all?
-	end
-
-	def persist
-		bank_entry.save
-	end
-
-	def group_is_active?
-		return true if @group.nil?
-		valid = ([bank_entry] + accounting_entries).map do |entry|
-			invalid = entry.date.present? && @group.is_inactive_on?(entry.date)
-			entry.errors.add(:date, :group_is_inactive_on_this_date) if invalid
-			!invalid
-		end
-		valid.all?
-	end
-
-	def has_at_least_one_accounting_entry?
-		return true if accounting_entries.length > 0
-		bank_entry.errors.add :accounting_entries, greater_than: 0
-		false
-	end
-
-	def amounts_match?
-		sum = accounting_entries.map(&:amount).inject(:+)
-		unless bank_entry.amount
-			bank_entry.amount = sum
-			true
-		else
-			return true if bank_entry.amount == sum
-			bank_entry.errors.add :amount, equal_to: sum
-			false
-		end
-	end
-
-	def respects_date_sequence?
-		return true if bank_entry.date.blank?
-		accounting_dates = accounting_entries.map(&:date).reject(&:blank?)
-		return true if accounting_dates.map{ |d| d <= bank_entry.date }.all?
-		bank_entry.errors.add :date, greater_than_or_equal_to: accounting_dates.sort.last
-		false
 	end
 
 end
